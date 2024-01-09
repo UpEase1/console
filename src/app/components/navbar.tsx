63f00d74cb2d058508504c0a2669430e54016ac9
Manikanth Martha <srimanikanth04@gmail.com>
2024-01-09 17:24:18 +0530

    basic routes

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'next/link'
+import students from '../students/page'
+
+
+const Navbar = () => {
+  return (
+    <nav className='w-full flex p-8 justify-between '>
+        <div>
+            <Link href='/' className=' text-2xl'>UpEase</Link>
+        </div>
+        <ul className=' text-2xl flex flex-col md:flex-row gap-8 '>
+            <li className='p-2.5 hover:bg-blue-100 hover:rounded-xl'><Link href='/' className=''>Copilot</Link></li>
+            <li className='p-2.5 hover:bg-blue-100 hover:rounded-xl'><Link href='/' className=''>Routines</Link></li>
+            <li className='p-2.5 hover:bg-blue-100 hover:rounded-xl'><Link href='../students' className=''>Students</Link></li>
+            <li className='p-2.5 hover:bg-blue-100 hover:rounded-xl'><Link href='../courses' className=''>Courses</Link></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
